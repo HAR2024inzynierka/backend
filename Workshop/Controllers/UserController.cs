2dac6535a6b7225f47069b80372e8089be215865
Artsiom <gwifen228322@gmail.com>
2024-10-28 15:21:39 +0100

    Added UserController

--- Workshop/Controllers/UserController.cs
+++ Workshop/Controllers/UserController.cs
@@ -1,6 +1,44 @@
-﻿namespace Workshop.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using Workshop.Core.Interfaces;
+using System.Threading.Tasks;
+using Workshop.Core.Entities;
+
+namespace Workshop.Controllers
 {
-    public class UserController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
     {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUser(int id)
+        {
+            var user = await _userService.GetUserByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(user);
+        }
+
+        [HttpPost("{userId}/vehicles")]
+        public async Task<IActionResult> AddVehicles(int userId, [FromBody] Vehicle vehicle)
+        {
+            if (vehicle == null)
+            {
+                return BadRequest();
+            }
+
+            vehicle.UserId = userId;
+            await _userService.AddVehicleAsync(vehicle);
+            return CreatedAtAction(nameof(AddVehicles), new { id = vehicle.Id }, vehicle);
+        }
     }
 }
